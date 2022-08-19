472449e8b7a590a5d13a1479cdc6fdf173cdcd46
Demid Malyanov <dagdog2506@gmail.com>
2022-08-19 11:40:45 +0500

    Update TodoStore.ts

--- src/store/TodoStore.ts
+++ src/store/TodoStore.ts
@@ -17,19 +17,16 @@ class TodoStore implements ITodoStore {
 
   addTodo(todo:ITodo) {
     this.todos.push(todo);
-    console.log("add", this.todos);
   }
 
   removeTodo(id:number) {
     this.todos = this.todos.filter((todo) => todo.id !== id);
-    console.log("remove", id);
   }
 
   completeTodo(id:number) {
     this.todos = this.todos.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
-    console.log("complete", id);
   }
 }
 
